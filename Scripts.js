9eb896f6bd9caecd5c423eaadfe43b422fe42bf9
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2022-03-04 20:27:43 +0000

    Add files via upload

--- Scripts.js
+++ Scripts.js
@@ -1,13 +1,21 @@
+//the placeholder holder
 var c;
+//the id of the morada
 var cc;
+//the innerHTML of morada
 var ccc;
+//input id
+var input;
 
+//this is for the list.js
 var options = {
-    valueNames: ['nome']
+    valueNames: ['nome', 'categoria']
 };
 
 var FirmList = new List('firms', options);
+//this is for the list.js
 
+//functions for search input
 function focuss(x) {
     c = x.placeholder;
     x.placeholder = "";
@@ -17,8 +25,45 @@ function blurr(x) {
     x.placeholder = c;
 }
 
+function Category(ategory) {
+    input = document.getElementById('searchinput');
+    input.value = '';
+    input.value = ategory.innerHTML;
+    document.getElementById('searchinput').focus();
+    var ev = new KeyboardEvent('keyup', {
+        altKey: false,
+        bubbles: true,
+        cancelBubble: false,
+        cancelable: true,
+        charCode: 0,
+        code: "Enter",
+        composed: true,
+        ctrlKey: false,
+        currentTarget: null,
+        defaultPrevented: true,
+        detail: 0,
+        eventPhase: 0,
+        isComposing: false,
+        isTrusted: true,
+        key: "Enter",
+        keyCode: 13,
+        location: 0,
+        metaKey: false,
+        repeat: false,
+        returnValue: false,
+        shiftKey: false,
+        type: "keyup",
+        which: 13
+    });
+
+    input.dispatchEvent(ev);
+}
+//functions for search input
+
+//buttons 'maps' function
 function Mapy(x) {
     cc = x.id - 1;
     ccc = document.getElementById(cc).innerHTML;
     window.location.href = "https://www.google.com/maps/place/" + ccc + '/';
 }
+//buttons 'maps' function
--- exercise.php
+++ exercise.php
@@ -11,35 +11,65 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <!-- bootstrap-->
-
-    <!-- List.js (it helps to search on website )-->
-    <script src="list.js"></script>
-    <!-- List.js (it helps to search on website )-->
-
-    <!-- My files -->
-    <link rel="stylesheet" href="style.css">
-    <script type="text/javascript" src="Scripts.js"></script>
-    <!-- My files -->
 </head>
 
-
-
-
 <body>
+
+<!-- This div is import for list.js work-->
     <div id='firms'>
-        <!-- input to search trought database -->
-        <input type="text" class="search" placeholder="Search" onfocus="focuss(this)" onblur="blurr(this)">
-        <!-- input to search trought database -->
-        
+        <!-- input to search trought website-->
+        <input type="select" id='searchinput' class="search" placeholder="Pesquisa, categoria, nome" onfocus="focuss(this)" onblur="blurr(this)">
+        <!-- input to search trought website-->
+        <!-- Categories -->
+        <button style='position:relative' class="btn" type="button" data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="collapseExample">
+            Categoria
+        </button>
+        <div class="collapse" id="collapse">
+            <div class="card card-body">
+
+                <p><button type="button" class="btn btn-secondary" onclick='Category(this)'>Informática</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Mecatronica</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Eletrónica</button></td>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Clínicas Veterinárias</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Quintas Animais</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Pastelaria</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Logística</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Têxtil</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>CNC</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Mecânica</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Aeroporto</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Estética</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Ginásios</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Turismo</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Restauração</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Publicidade</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Expedição</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Alarmes</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Cabeleireiros</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Construção</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Associações</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Serralharia</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Carpintaria</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Comércio</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Economia</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Arquitetura paisagistica</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Imobiliária</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Arte</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Química</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Topografos</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Impressão</button>
+                    <button type="button" class="btn btn-secondary" onclick='Category(this)'>Flexografia</button>
+                </p>
+            </div>
+        </div>
+        <!-- Categories -->
+
         <!-- Table where the results are shown-->
         <table class="table table-hover">
             <thead>
                 <tr>
                     <th scope="col">NOME</th>
-                    <th scope="col">TLM</th>
                     <th scope="col">MORADA</th>
-                    <th scope="col">HORÁRIO</th>
-                    <th scope="col">Observações</th>
                     <th scope="col">Google Maps</th>
                 </tr>
             </thead>
@@ -52,31 +82,38 @@
                     '',
                     'drivers project'
                 );
-
                 $querry = "SELECT * FROM companies_contacts ";
                 $result = mysqli_query($conn, $querry);
+                // 'n' is the special id that is asigned to the morada and maps 
                 $n = 0;
+                //Loop to show all of the sql tables
                 while ($row = mysqli_fetch_array($result)) {
-
                     echo "<tr>" .
-                        "<td class = 'nome'>" . htmlspecialchars($row['NOME']) .
-                        "<td class = 'TLM'>" . htmlspecialchars($row['TLM']) .
-                        "<td class = 'morada' id = '$n'>" . htmlspecialchars($row['MORADA']);
+                        "<td class = 'categoria'>" . htmlspecialchars($row['Categoria']) . "</td>" .
+                        "<td class = 'nome'>" . htmlspecialchars($row['NOME']) . "</td>" .
+                        "<td id = '$n'>" . htmlspecialchars($row['MORADA']) . "</td>";
                     $n = $n + 1;
-                    echo "<td class = 'hor_rio'>" . htmlspecialchars($row['HOR_RIO']) .
-                        "<td class = 'observa_es' >" . htmlspecialchars($row['Observa_es']) .
-                        "<td><button id = '$n' onclick='Mapy(this)'>MAPS</button> </td>
+                    echo "<td><button id = '$n' onclick='Mapy(this)'>MAPS</button> </td>
                     </tr>";
                     $n = $n + 1;
                 }
-                $n = $n * 10;
-                echo $n;
 
 
                 ?>
             </tbody>
         </table>
+        <!-- Table where the results are shown-->
     </div>
+<!-- This div is import for list.js work-->
+
+    <!-- List.js (it helps to search on the website modifying table in realtime )-->
+    <script src="list.js"></script>
+    <!-- List.js (it helps to search on the website modifying table in realtime )-->
+
+    <!-- My files -->
+    <script type="text/javascript" src="Scripts.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <!-- My files -->
 </body>
 
 </html>
